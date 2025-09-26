8dc4efe8cb6f99b05c22edc512c1e3d67c280e0f
Jeferson <jyaxon-2024442@kinal.edu.gt>
2025-09-25 21:41:40 -0600

    004 se completaron los ejercicios

---
+++ lab-tres/Ejercicio1-Jeferson_Yaxon_2024442.js
@@ -0,0 +1,9 @@
+function verificarNumero(num) {
+    if (num > 0) {
+        return "Positivo";
+    } else if (num < 0) {
+        return "Negativo";
+    } else {
+        return "Cero";
+    }
+}
